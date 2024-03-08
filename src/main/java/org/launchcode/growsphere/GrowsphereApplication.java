3a6d4c345769f27411e6b96e20556ac6669b7629
stephenmdevine <stephenmdevine@gmail.com>
2024-03-08 11:36:01 -0600

    Spring initialization

---
+++ src/main/java/org/launchcode/growsphere/GrowsphereApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.growsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowsphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowsphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/growsphere/GrowsphereApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.growsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowsphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
