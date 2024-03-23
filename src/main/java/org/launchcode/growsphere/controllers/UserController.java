9b9a5c6f9b903bac0ba783248d070ef7df584716
stephenmdevine <stephenmdevine@gmail.com>
2024-03-22 23:12:48 -0500

    Front-end and Back-end connected

--- src/main/java/org/launchcode/growsphere/controllers/UserController.java
+++ src/main/java/org/launchcode/growsphere/controllers/UserController.java
@@ -3,14 +3,12 @@ package org.launchcode.growsphere.controllers;
 import org.launchcode.growsphere.data.UserRepository;
 import org.launchcode.growsphere.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000/")
 public class UserController {
 
     @Autowired
