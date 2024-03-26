06924bcccb34d542f2b27a9ef8ca3962b31e2a2f
stephenmdevine <stephenmdevine@gmail.com>
2024-03-25 19:53:45 -0500

    Models updated

--- src/main/java/org/launchcode/growsphere/models/Plant.java
+++ src/main/java/org/launchcode/growsphere/models/Plant.java
@@ -12,30 +12,101 @@ public class Plant extends AbstractEntity {
 
     private String scientificName;
 
-    @ManyToOne
-    private PlantType plantType;
+    private String plantType;
 
-    // TODO: Water Requirements should use a date format?
-    @ManyToOne
-    private WaterReqs waterRequirements;
+    private String waterRequirements;
 
-    // TODO: Sow could use a date format?
     private String sow;
 
-    // TODO: Harvest could use a date format?
     private String harvest;
 
-    @ManyToOne
-    private SunExposure exposure;
+    private String exposure;
 
-    private double phLevel;
+    private String phLevel;
 
-    // TODO: Fertilizer should use a date format?
     private String fertilizer;
 
     @ManyToMany(mappedBy = "plants")
     private final List<User> users = new ArrayList<>();
 
-    // TODO: Getters, setters, toString
+    public String getCommonName() {
+        return commonName;
+    }
 
+    public void setCommonName(String commonName) {
+        this.commonName = commonName;
+    }
+
+    public String getScientificName() {
+        return scientificName;
+    }
+
+    public void setScientificName(String scientificName) {
+        this.scientificName = scientificName;
+    }
+
+    public String getPlantType() {
+        return plantType;
+    }
+
+    public void setPlantType(String plantType) {
+        this.plantType = plantType;
+    }
+
+    public String getWaterRequirements() {
+        return waterRequirements;
+    }
+
+    public void setWaterRequirements(String waterRequirements) {
+        this.waterRequirements = waterRequirements;
+    }
+
+    public String getSow() {
+        return sow;
+    }
+
+    public void setSow(String sow) {
+        this.sow = sow;
+    }
+
+    public String getHarvest() {
+        return harvest;
+    }
+
+    public void setHarvest(String harvest) {
+        this.harvest = harvest;
+    }
+
+    public String getExposure() {
+        return exposure;
+    }
+
+    public void setExposure(String exposure) {
+        this.exposure = exposure;
+    }
+
+    public String getPhLevel() {
+        return phLevel;
+    }
+
+    public void setPhLevel(String phLevel) {
+        this.phLevel = phLevel;
+    }
+
+    public String getFertilizer() {
+        return fertilizer;
+    }
+
+    public void setFertilizer(String fertilizer) {
+        this.fertilizer = fertilizer;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    @Override
+    public String toString() {
+        return commonName;
+    }
 }
--- src/main/java/org/launchcode/growsphere/services/PlantService.java
+++ src/main/java/org/launchcode/growsphere/services/PlantService.java
@@ -1,4 +1,7 @@
 package org.launchcode.growsphere.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PlantService {
 }
